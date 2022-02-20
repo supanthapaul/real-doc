14892404cfcf491c53c186b9cab1bfcf558a7f42
Supantha Paul <supapaul56@gmail.com>
2022-02-20 23:29:59 +0530

    Added document persistance on mongodb

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
+require("dotenv").config();
 const { Server } = require("socket.io");
+const mongoose = require('mongoose');
+const Document = require('./models/Document');
+
+mongoose.connect(process.env.MONGOURL);
 
 const io = new Server(3001, {
 	cors: {
@@ -11,17 +16,29 @@ io.on("connection", socket => {
 
 	console.log("Connected");
 
-	socket.on('get-document', documentId => {
-		const data = "";
+	socket.on('get-document', async documentId => {
+		const document= await findOrCreateDocument(documentId);
 		socket.join(documentId);
 		// emit the event with data from database
-		socket.emit('load-document', data);
+		socket.emit('load-document', document);
 	
 		socket.on('send-changes', delta => {
 			// broadcast the changes to everyone else in the same room
 			socket.broadcast.to(documentId).emit('receive-changes', delta);
 		})
-	})
 
-	
-})
+		socket.on('save-document', async data => {
+			await Document.findByIdAndUpdate(documentId, { data });
+		})
+	})
+});
+
+async function findOrCreateDocument(id) {
+	if(id == null) return;
+
+	const document = await Document.findById(id);
+	//console.log(document)
+	if(document) return document;
+
+	return await Document.create({_id: id, title: "New Document", data: ""});
+}
